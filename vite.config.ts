42d6558a7ad1ba17a08c31bf43a5968f92c61477
luuboon <abrajam285@gmail.com>
2025-11-09 05:51:27 -0600

    Fix: Cambiar base a ruta absoluta para Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/',
   resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
